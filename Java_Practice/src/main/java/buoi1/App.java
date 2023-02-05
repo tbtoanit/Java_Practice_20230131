ca26751c37e69dfdd01628e7f59d276faef3e25a
toantranbao <drivervaccine@gmail.com>
2023-02-05 19:06:03 +0700

    Rollback Email "Request modify email title"

--- Java_Practice/src/main/java/buoi1/App.java
+++ Java_Practice/src/main/java/buoi1/App.java
@@ -4,14 +4,12 @@ package buoi1;
  * Hello world!
  *
  */
-//Gitlap
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         System.out.println("This is my line");
-        //làm 1 tính năng mới
     }
 
 }
