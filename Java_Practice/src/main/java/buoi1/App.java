fd609661b7697caab190016294ad9a7a5ce766b7
toantranbao <drivervaccine@gmail.com>
2023-02-05 18:49:38 +0700

    Email "Request modify email title"

--- Java_Practice/src/main/java/buoi1/App.java
+++ Java_Practice/src/main/java/buoi1/App.java
@@ -4,12 +4,14 @@ package buoi1;
  * Hello world!
  *
  */
+//Gitlap
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         System.out.println("This is my line");
+        //làm 1 tính năng mới
     }
 
 }
