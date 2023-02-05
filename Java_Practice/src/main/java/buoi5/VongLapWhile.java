878dce5b414033f11caa6881c7c8ce5a19a2f2d3
Trianh0406 <rylailina601@gmail.com>
2023-02-05 19:16:59 +0700

    Request language change

--- Java_Practice/src/main/java/buoi5/VongLapWhile.java
+++ Java_Practice/src/main/java/buoi5/VongLapWhile.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class VongLapWhile {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nhập 'yes' nếu bạn muốn thoát chương trình: ");
+        System.out.println("Press 'yes' if you want to exit the program ");
         String existFlag = "no";
 
 //        while(true){
